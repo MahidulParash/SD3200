5433741fffaf115cd83a373bc867e0f9dd9c8cb3
Munem-Prionto <munem.prionto@gmail.com>
2022-07-12 22:43:05 +0600

    Add project files.

---
+++ SD3200/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SD3200.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
